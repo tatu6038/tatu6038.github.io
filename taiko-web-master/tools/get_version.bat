b41638d4c4af6de33e7b6e73feabde8df48990b7
tatu#6038 <93127302+tatu6038@users.noreply.github.com>
2021-12-15 23:16:15 +0900

    Delete taiko-web-master directory

--- taiko-web-master/tools/get_version.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-(
-git log -1 --pretty="format:{\"commit\": \"%%H\", \"commit_short\": \"%%h\", \"version\": \"%%ad\"}" --date="format:%%y.%%m.%%d"
-) > ../version.json
--- taiko-web-master/tools/get_version.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-toplevel=$( git rev-parse --show-toplevel )
-git log -1 --pretty="format:{\"commit\": \"%H\", \"commit_short\": \"%h\", \"version\": \"%ad\"}" --date="format:%y.%m.%d" > "$toplevel/version.json"
