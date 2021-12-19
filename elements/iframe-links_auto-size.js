5e92f7c9d13c95ba58c2940efa6a4563614ba85b
tatu#6038 <93127302+tatu6038@users.noreply.github.com>
2021-12-19 18:27:08 +0900

    Update iframe-links_auto-size.js

--- elements/iframe-links_auto-size.js
+++ elements/iframe-links_auto-size.js
@@ -1,6 +1,6 @@
-const iframe = document.getElementById("iframe");
-function funciframe()
+const links = document.getElementById("links");
+function funclinks()
 {
-  iframe.style.width = iframe.contentWindow.document.body.scrollWidth + "px";
-  iframe.style.height = iframe.contentWindow.document.body.scrollHeight + "px";
+  links.style.width = links.contentWindow.document.body.scrollWidth + "px";
+  links.style.height = links.contentWindow.document.body.scrollHeight + "px";
 }
