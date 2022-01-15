08d39b229853564d03d729e76b5da8fad2b1901c
tatu#6038 <93127302+tatu6038@users.noreply.github.com>
2022-01-15 19:43:13 +0900

    Delete elements directory

--- elements/iframe-header_auto-size.js
+++
@@ -1,6 +0,0 @@
-const header = document.getElementById("header");
-function funcheader()
-{
-  header.style.width = header.contentWindow.document.body.scrollWidth + "px";
-  header.style.height = header.contentWindow.document.body.scrollHeight + "px";
-}
--- elements/iframe-links_auto-size.js
+++
@@ -1,6 +0,0 @@
-const links = document.getElementById("links");
-function funclinks()
-{
-  links.style.width = links.contentWindow.document.body.scrollWidth + "px";
-  links.style.height = links.contentWindow.document.body.scrollHeight + "px";
-}
