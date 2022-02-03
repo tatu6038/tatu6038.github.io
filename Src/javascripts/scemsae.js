c7adb59f72711cb2531f84cd2472c8bc29f4e81e
tatu#6038 <93127302+tatu6038@users.noreply.github.com>
2022-02-03 21:13:27 +0900

    Create scemsae.js

---
+++ Src/javascripts/scemsae.js
@@ -0,0 +1,20 @@
+function kksmsae(){
+  alert("-secret message-");
+  alert("I regret and feel bad about that ploblem that I caused.");
+  alert("Sorry about that.");
+  alert("I wish I was brave to apologize to you for that…");
+  alert("Let me explain that what did I do.");
+  alert("First, I loved you purely.");
+  alert("But, I forget that I had heard a little about you and him dating at that time that I told that I liked you.");
+  alert("Second, I think that you are almost perfectly capable of doing all of things.");
+  alert("But, I can only assume that I'm a flaw.");
+  alert("So, I thought that we aren't balance and I thought that I can't be for you.");
+  alert("Third,I have no room in my heart.");
+  alert("Then you shouldn't have said you liked me.");
+  alert("I'm sure you'll think so…");
+  alert("Now, I want to get back together again.");
+  alert("But, I know that there isn't convenient because I know that you have your reasons.");
+  alert("In reality, I am sad to know, you will never read this sentence…");
+  alert("For these reasons, all I can do that I keep worrying:_(");
+  alert("But don't worry, I'm okey. I hope your happy.");
+}
