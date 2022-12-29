8a817a60438ec2f918738ae4eba450042b8b3702
tatu#6038 <93127302+tatu6038@users.noreply.github.com>
2022-12-29 22:16:25 +0900

    計算機の追加とゲーム修正
    - 計算機v0.1の追加(動かない)
    - 各シンプルゲームの修正

---
+++ Src/javascripts/ele_calc.js
@@ -0,0 +1,12 @@
+var sym = 0;
+var dot = 0;
+var decCnt = 0;
+function ac(){
+    numOut = 0;
+    sym = 0;
+    dot = 0;
+    decCnt = 0;
+    let text = document.getElementsByClassName("txt").textContent;
+    document.getElementsByClassName("txt").textContent = "書き換え成功";
+    console.log("ac終了")
+}
